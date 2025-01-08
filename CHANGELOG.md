1141d826cae35be4f5db7f9835f917020024a384
Tamas Kis <tamas.a.kis@outlook.com>
2025-01-08 00:06:49 -0500

    RK4 (#3)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.1.1
+
+1. Added `RK4` integration method.
+
 ## 0.1.0
 
 1. Initial release.
--- src/integration_methods/runge_kutta.rs
+++ src/integration_methods/runge_kutta.rs
@@ -7,10 +7,30 @@ pub struct Euler;
 
 impl<T: OdeState> IntegrationMethod<T> for Euler {
     fn propagate(f: &impl Fn(f64, &T) -> T, t: f64, h: f64, y: &mut T) {
-        // y(t + h) = y(t) + hf(t, y)
-        let mut f_eval = f(t, y);
-        f_eval.mul_assign(h);
-        y.add_assign(&f_eval);
+        // yₙ₊₁ = yₙ + hf(tₙ, yₙ)
+        y.add_assign(&f(t, y).mul(h));
+    }
+}
+
+/// (Classic) Runge-Kutta fourth-order method.
+pub struct RK4;
+
+impl<T: OdeState> IntegrationMethod<T> for RK4 {
+    fn propagate(f: &impl Fn(f64, &T) -> T, t: f64, h: f64, y: &mut T) {
+        // k₁ = f(tₙ, yₙ)
+        let k1 = f(t, y);
+
+        // k₂ = f(tₙ + (h/2), yₙ + (hk₁/2))
+        let k2 = f(t + (h / 2.0), &y.add(&k1.mul(h / 2.0)));
+
+        // k₃ = f(tₙ + (h/2), yₙ + (hk₂/2))
+        let k3 = f(t + (h / 2.0), &y.add(&k2.mul(h / 2.0)));
+
+        // k₄ = f(tₙ + h, yₙ + hk₃)
+        let k4 = f(t + h, &y.add(&k3.mul(h)));
+
+        // yₙ₊₁ = yₙ = (h/6)(k₁ + 2k₂ + 2k₃ + k₄)
+        y.add_assign(&(k1.add(&k2.mul(2.0)).add(&k3.mul(2.0)).add(&k4)).mul(h / 6.0));
     }
 }
 
@@ -18,10 +38,22 @@ impl<T: OdeState> IntegrationMethod<T> for Euler {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_euler_scalar() {
+    /// Helper function for testing Runge-Kutta integration methods.
+    ///
+    /// # Type Parameters
+    ///
+    /// * `T` - Integration method type.
+    ///
+    /// # Arguments
+    ///
+    /// * `y_exp` - Expected value of the ODE state after one propagation.
+    ///
+    /// # Panics
+    ///
+    /// * If the ODE state after one propagation does not equal `y_exp`.
+    fn rkx_test_helper<T: IntegrationMethod<f64>>(y_exp: f64) {
         // Function defining the ODE.
-        let f = |_t: f64, x: &f64| -2.0 * x;
+        let f = |t: f64, x: &f64| -2.0 * x + t;
 
         // Current state.
         let mut y = 1.0;
@@ -33,9 +65,19 @@ mod tests {
         let h = 0.1;
 
         // Propagate using the Euler method.
-        Euler::propagate(&f, t, h, &mut y);
+        T::propagate(&f, t, h, &mut y);
 
         // Check value after one propagation.
-        assert_eq!(y, 0.8);
+        assert_eq!(y, y_exp);
+    }
+
+    #[test]
+    fn test_euler() {
+        rkx_test_helper::<Euler>(0.8);
+    }
+
+    #[test]
+    fn test_rk4() {
+        rkx_test_helper::<RK4>(0.8234166666666667);
     }
 }
